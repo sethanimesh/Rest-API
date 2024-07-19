fdb15ecc7c6bffa186217e3bc8d3661c2cf3f67c
animesh <sethanimesh@hotmail.com>
2024-07-19 09:19:25 +0530

    rest api with path variables

---
+++ src/main/java/com/sethanimesh/rest/webservices/restful_web_services/helloworld/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package com.sethanimesh.rest.webservices.restful_web_services.helloworld;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.setMessage(message);
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+	
+}
+
+
