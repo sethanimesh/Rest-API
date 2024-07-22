ac53cdefbe84cc46fa6139587a969622deb212f6
animesh <sethanimesh@hotmail.com>
2024-07-22 21:57:46 +0530

    filtering

--- src/main/java/com/sethanimesh/rest/webservices/restful_web_services/user/User.java
+++ src/main/java/com/sethanimesh/rest/webservices/restful_web_services/user/User.java
@@ -2,6 +2,8 @@ package com.sethanimesh.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -11,9 +13,11 @@ public class User {
 	private Integer id;
 	
 	@Size(min=2, message="Name should have atleast 2 characters")
+	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message="Birth Date should always be in the past")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
